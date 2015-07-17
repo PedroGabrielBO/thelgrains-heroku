0e0155d6b9d9e59351d1711c556c67a89042d85c
Victor Silva <victorsilva223@gmail.com>
2015-07-17 01:01:27 -0300

    correção no sql web comentario

--- app/models/web_comentario.rb
+++ app/models/web_comentario.rb
@@ -9,7 +9,7 @@ class WebComentario < ActiveRecord::Base
     # TODO query de verificação de limite de WebComentarios (parametros de configuração)
     n = 2 # quantidade limite de comentarios (exclusive)
     t = 10 # tempo em minutos (exclusive)
-    query = WebComentario.where([ "updated_at > ? AND email == ?", t.minutes.ago, email ])
+    query = WebComentario.where([ "updated_at > ? AND email = ?", t.minutes.ago, email ]) # :updated_at > t.minutes.ago, email: email
     puts(query.size)
     if query.size > n
       query.each { |a| a.touch }
