0f330803b4d692a5ad58345661808a39b588eb6c
Victor Silva <victorsilva223@gmail.com>
2015-07-17 21:28:21 -0300

    correcao

--- app/controllers/web_comentarios_controller.rb
+++ app/controllers/web_comentarios_controller.rb
@@ -1,5 +1,5 @@
 class WebComentariosController < ApplicationController
-  # skip_before_filter :verify_authenticity_token
+  skip_before_filter :verify_authenticity_token
   after_filter { flash.discard if request.xhr? }
 
   def create
